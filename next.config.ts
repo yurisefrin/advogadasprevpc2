e47726c65f506f5127c0e534ae7be2a3ec6769bd
Yuri Fernando Tiecher Sefrin <yurisefrin@gmail.com>
2026-06-17 15:18:58 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      { source: "/salario-maternidade", destination: "/salario-maternidade.html" },
+    ];
+  },
 };
 
 export default nextConfig;
